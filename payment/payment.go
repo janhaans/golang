4a2e28b5ea894d79f1f8a3d355803b30e6ebf5a3
Jan Haans <janhaans@Jans-MacBook-Air.local>
2023-10-31 18:05:53 +0100

    Idemptotent payment processing

--- payment/payment.go
+++ payment/payment.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Payment struct {
+	From   string
+	To     string
+	Amount float64
+	once   sync.Once
+}
+
+func (p *Payment) process(t time.Time) {
+	fmt.Printf("[%s] Payment: %s -> %.2f€ -> %s\n", t.Format(time.RFC3339), p.From, p.Amount, p.To)
+}
+
+func (p *Payment) Process() {
+	now := time.Now()
+	p.once.Do(func() {
+		p.process(now)
+	})
+}
 
 func main() {
-	fmt.Println("hello")
+	p := Payment{
+		From:   "Jan",
+		To:     "Anna",
+		Amount: 123.45,
+	}
+
+	p.Process()
+	p.Process()
 }
